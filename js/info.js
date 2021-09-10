8abadd444e89c29128ee2ab28792bfdcf936f2da
Katechi <katerinne.leiva@gmail.com>
2021-09-10 18:00:50 -0300

    implementación información de estado
    Se implementa la información de estado en un cuadro a través de javascript, dependiendo de su modo (setup, record o playblack)

--- js/info.js
+++ js/info.js
@@ -1,21 +1,35 @@
 const infoButton = document.querySelector("#info")
-const infoStateDiv = document.querySelector(".infoState")
+    //const infoStateDiv = document.querySelector(".infoState")
+const infostate = document.querySelector("#infostate")
 
 infoButton.addEventListener("click", () => {
     var cmd = infoButton.getAttribute("cmd")
     var par = infoButton.getAttribute("par")
 
     fetch("/system/station1/dhs1", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({cmd: cmd, par: par})
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        const {status, mode, time} = data
-        infoStateDiv.innerHTML = `Status: ${status},Mode: ${mode},Time: ${time}`
-    })
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ cmd: cmd, par: par })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.mode)
+            console.log(cmd)
+
+
+            const { status, mode, time, fri, frt, frc } = data
+            if (data.mode == 'playback') {
+                infostate.innerHTML = `Status: ${status}
+                ,Mode: ${mode},
+                Time: ${time},
+                Frame index: ${fri},
+                Frame Time: ${frt},
+                Frame count: ${frc}`
+            } else {
+
+                infostate.innerHTML = `Status: ${status},Mode: ${mode},Time: ${time}`
+            }
+        })
 })
