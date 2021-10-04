d04c376cfcb9d23c872cc87cbff9aa4ce4c6d920
Jean Pacheco <paulcitronico@gmail.com>
2021-10-04 19:10:56 -0300

    sinc. boton
    botones first, last, prev y next con seeks frames

--- js/playBack.js
+++ js/playBack.js
@@ -31,24 +31,28 @@ prev_Button.addEventListener("click", () =>{
     var command =prev_Button.getAttribute("cmd")
     var par=prev_Button.getAttribute("par")
     fetch_playBack(command,par)
+    move_seek_with_playbakButton()
 })
 
 next_Button.addEventListener("click", () =>{
     var command =next_Button.getAttribute("cmd")
     var par=next_Button.getAttribute("par")
     fetch_playBack(command,par)
+    move_seek_with_playbakButton()
 })
 
 first_Button.addEventListener("click", () =>{
     var command =first_Button.getAttribute("cmd")
     var par=first_Button.getAttribute("par")
     fetch_playBack(command,par)
+    move_seek_with_playbakButton()
 })
 
 last_Button.addEventListener("click", () =>{
     var command = last_Button.getAttribute("cmd")
     var par=last_Button.getAttribute("par")
-    fetch_playBack(command,par);
+    fetch_playBack(command,par)
+    move_seek_with_playbakButton()
 })
 
 seek_norm_input.addEventListener("change", () => {
@@ -86,4 +90,43 @@ async function fetch_seek_norm(value){
         body: JSON.stringify({cmd:"seek_norm",par: value/1000.0})
     })
     console.log({"Ok": response.ok, "Status": response.status, "Message": "Seek Norm"})
+}
+
+async function move_seek_with_playbakButton(){
+    fetch("/system/station1/dhs1",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({cmd:"info",par:""})
+    })
+    .then(res => res.json())
+    .then(data => {
+        const {mode,frc,fri} = data
+        if (mode === "playback"){
+            // Seek to frame by index
+            frame_index_input.setAttribute("min",0)
+            frame_index_input.setAttribute("max",frc-1)
+            frame_index_input.value = fri
+            document.querySelector("#seek_frame_index").innerHTML = frame_index_input.value
+            frame_index_input.addEventListener("change",async () => {
+                document.querySelector("#seek_frame_index").innerHTML = frame_index_input.value
+                seek("seek_to_frame",frame_index_input.value)
+                frame_time_input.value = frame_index_input.value
+                document.querySelector("#seek_frame_time").innerHTML = frame_time_input.value/22000.0
+            })
+            // Seek to frame by time
+            frame_time_input.setAttribute("min",0)
+            frame_time_input.setAttribute("max",(frc-1))
+            frame_time_input.value = fri
+            document.querySelector("#seek_frame_time").innerHTML = frame_time_input.value/22000.0
+            frame_time_input.addEventListener("change", async () => {
+                document.querySelector("#seek_frame_time").innerHTML = frame_time_input.value/22000.0
+                seek("seek_to_time",frame_time_input.value/22000.0)
+                frame_index_input.value = frame_time_input.value
+                document.querySelector("#seek_frame_index").innerHTML = frame_index_input.value
+            })
+        }
+    })
+    .catch(err => console.log(err))
 }
