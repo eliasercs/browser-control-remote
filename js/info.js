d07de9db68f98b0f7bd02eeb9ec9ba1cb68b1c1d
Katechi <katerinne.leiva@gmail.com>
2021-11-09 00:11:43 -0300

    Reestructuración seek y sincronizacion info
    Reestructuración de componentes seek y sincronizacion de información de estado con frame.

--- js/info.js
+++ js/info.js
@@ -1,46 +1,273 @@
+var play = false
 const infostate = document.querySelector("#infostate")
 
-document.querySelector("#infoButton").addEventListener("click", () => {
-
-  fetch(`/system/${document.querySelector("#stations").value}/dhs1`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ cmd: "info", par: "" })
-  })
-    .then(res => res.json())
-    .then(data => {
-      const { status, mode, time, fri, frt, frc } = data
-      console.log({ "status": status })
-      var frame = `
+function requestinfo() {
+    if (play) {
+        info()
+            .then(res => res.json())
+            .then(data => {
+                const { status, mode, time, fri, frt, frc } = data
+                console.log({ "status": status })
+                var frame = `
+        <tr>
+          <td>Frame Index</td>
+          <td>${fri}</td>
+        </tr>
       <tr>
-        <td>Frame Index</td>
-        <td>${fri}</td>
+        <td>Frame Time</td>
+        <td>${frt}</td>
       </tr>
+      <tr>
+        <td>Frame Count</td>
+        <td>${frc}</td>
+      </tr>`
+
+                infostate.innerHTML = `
+              <tr>
+                <td>Status</td>
+                <td>${status}</td>
+              </tr>
+              <tr>
+                <td>Mode</td>
+                <td>${mode}</td>
+              </tr>
+              <tr>
+                <td>Time</td>
+                <td>${time}</td>
+              </tr>
+              ${mode === "playback" ? frame : "" }
+              `
+            })
+    }
+}
+
+document.querySelector("#play").addEventListener("click", () => {
+    if (document.querySelector("#stations").value !== "default") {
+        play = true
+        setInterval("requestinfo()", 250)
+    } else {
+        alert("You must select a station.")
+    }
+})
+
+document.querySelector("#stop").addEventListener("click", () => {
+    if (document.querySelector("#stations").value !== "default") {
+        play = false
+        info()
+            .then(res => res.json())
+            .then(data => {
+                const { status, mode, time, fri, frt, frc } = data
+                console.log({ "status": status })
+                var frame = `
+  <tr>
+    <td>Frame Index</td>
+    <td>${fri}</td>
+  </tr>
+<tr>
+  <td>Frame Time</td>
+  <td>${frt}</td>
+</tr>
+<tr>
+  <td>Frame Count</td>
+  <td>${frc}</td>
+</tr>`
+
+                infostate.innerHTML = `
+        <tr>
+          <td>Status</td>
+          <td>${status}</td>
+        </tr>
+        <tr>
+          <td>Mode</td>
+          <td>${mode}</td>
+        </tr>
+        <tr>
+          <td>Time</td>
+          <td>${time}</td>
+        </tr>
+        ${mode === "playback" ? frame : "" }
+        `
+            })
+            .catch(err => console.log(err))
+    } else {
+        alert("You must select a station.")
+    }
+})
+
+
+document.querySelector("#last").addEventListener("click", () => {
+    if (document.querySelector("#stations").value !== "default") {
+        play = false
+        info()
+            .then(res => res.json())
+            .then(data => {
+                const { status, mode, time, fri, frt, frc } = data
+                console.log({ "status": status })
+                var frame = `
+<tr>
+  <td>Frame Index</td>
+  <td>${fri}</td>
+</tr>
+<tr>
+<td>Frame Time</td>
+<td>${frt}</td>
+</tr>
+<tr>
+<td>Frame Count</td>
+<td>${frc}</td>
+</tr>`
+
+                infostate.innerHTML = `
+      <tr>
+        <td>Status</td>
+        <td>${status}</td>
+      </tr>
+      <tr>
+        <td>Mode</td>
+        <td>${mode}</td>
+      </tr>
+      <tr>
+        <td>Time</td>
+        <td>${time}</td>
+      </tr>
+      ${mode === "playback" ? frame : "" }
+      `
+            })
+            .catch(err => console.log(err))
+    } else {
+        alert("You must select a station.")
+    }
+})
+
+document.querySelector("#prev").addEventListener("click", () => {
+    if (document.querySelector("#stations").value !== "default") {
+        play = false
+        info()
+            .then(res => res.json())
+            .then(data => {
+                const { status, mode, time, fri, frt, frc } = data
+                console.log({ "status": status })
+                var frame = `
+<tr>
+<td>Frame Index</td>
+<td>${fri}</td>
+</tr>
+<tr>
+<td>Frame Time</td>
+<td>${frt}</td>
+</tr>
+<tr>
+<td>Frame Count</td>
+<td>${frc}</td>
+</tr>`
+
+                infostate.innerHTML = `
     <tr>
-      <td>Frame Time</td>
-      <td>${frt}</td>
+      <td>Status</td>
+      <td>${status}</td>
     </tr>
     <tr>
-      <td>Frame Count</td>
-      <td>${frc}</td>
-    </tr>`
+      <td>Mode</td>
+      <td>${mode}</td>
+    </tr>
+    <tr>
+      <td>Time</td>
+      <td>${time}</td>
+    </tr>
+    ${mode === "playback" ? frame : "" }
+    `
+            })
+            .catch(err => console.log(err))
+    } else {
+        alert("You must select a station.")
+    }
+})
 
-      infostate.innerHTML = `
-            <tr>
-              <td>Status</td>
-              <td>${status}</td>
-            </tr>
-            <tr>
-              <td>Mode</td>
-              <td>${mode}</td>
-            </tr>
-            <tr>
-              <td>Time</td>
-              <td>${time}</td>
-            </tr>
-            ${mode === "playback" ? frame : "" }
-            `
-    })
+
+document.querySelector("#next").addEventListener("click", () => {
+    if (document.querySelector("#stations").value !== "default") {
+        play = false
+        info()
+            .then(res => res.json())
+            .then(data => {
+                const { status, mode, time, fri, frt, frc } = data
+                console.log({ "status": status })
+                var frame = `
+<tr>
+<td>Frame Index</td>
+<td>${fri}</td>
+</tr>
+<tr>
+<td>Frame Time</td>
+<td>${frt}</td>
+</tr>
+<tr>
+<td>Frame Count</td>
+<td>${frc}</td>
+</tr>`
+
+                infostate.innerHTML = `
+  <tr>
+    <td>Status</td>
+    <td>${status}</td>
+  </tr>
+  <tr>
+    <td>Mode</td>
+    <td>${mode}</td>
+  </tr>
+  <tr>
+    <td>Time</td>
+    <td>${time}</td>
+  </tr>
+  ${mode === "playback" ? frame : "" }
+  `
+            })
+            .catch(err => console.log(err))
+    } else {
+        alert("You must select a station.")
+    }
+})
+
+document.querySelector("#first").addEventListener("click", () => {
+    if (document.querySelector("#stations").value !== "default") {
+        play = false
+        info()
+            .then(res => res.json())
+            .then(data => {
+                const { status, mode, time, fri, frt, frc } = data
+                console.log({ "status": status })
+                var frame = `
+<tr>
+<td>Frame Index</td>
+<td>${fri}</td>
+</tr>
+<tr>
+<td>Frame Time</td>
+<td>${frt}</td>
+</tr>
+<tr>
+<td>Frame Count</td>
+<td>${frc}</td>
+</tr>`
+
+                infostate.innerHTML = `
+  <tr>
+    <td>Status</td>
+    <td>${status}</td>
+  </tr>
+  <tr>
+    <td>Mode</td>
+    <td>${mode}</td>
+  </tr>
+  <tr>
+    <td>Time</td>
+    <td>${time}</td>
+  </tr>
+  ${mode === "playback" ? frame : "" }
+  `
+            })
+            .catch(err => console.log(err))
+    } else {
+        alert("You must select a station.")
+    }
 })
--- js/main.js
+++ js/main.js
@@ -39,12 +39,12 @@ terminate_button.addEventListener("click", () => {
 document.querySelector("#stations").addEventListener("change", () => {
     if (document.querySelector("#stations").value !== "default") {
         fetch(`/system/${document.querySelector("#stations").value}/dhs1`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ cmd: "info", par: "" })
-        })
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ cmd: "info", par: "" })
+            })
             .then(res => res.json())
             .then(data => {
                 const { mode, frc, fri } = data
@@ -57,32 +57,69 @@ document.querySelector("#stations").addEventListener("change", () => {
                         var urlCreator = window.URL || window.webkitURL
                         document.querySelector("#frameImage").setAttribute("src", urlCreator.createObjectURL(file))
                     })
-                    .catch(err => console.log(err))
+
+                info()
+                    .then(res => res.json())
+                    .then(data => {
+                        const { status, mode, time, fri, frt, frc } = data
+                        console.log({ "status": status })
+                        var frame = `
+        <tr>
+          <td>Frame Index</td>
+          <td>${fri}</td>
+        </tr>
+      <tr>
+        <td>Frame Time</td>
+        <td>${frt}</td>
+      </tr>
+      <tr>
+        <td>Frame Count</td>
+        <td>${frc}</td>
+      </tr>`
+
+                        infostate.innerHTML = `
+              <tr>
+                <td>Status</td>
+                <td>${status}</td>
+              </tr>
+              <tr>
+                <td>Mode</td>
+                <td>${mode}</td>
+              </tr>
+              <tr>
+                <td>Time</td>
+                <td>${time}</td>
+              </tr>
+              ${mode === "playback" ? frame : "" }
+              `
+                    })
+
+                .catch(err => console.log(err))
 
                 if (mode === "playback") {
                     // Seek to frame by index
                     frame_index_input.setAttribute("min", 0)
                     frame_index_input.setAttribute("max", frc - 1)
                     frame_index_input.value = fri
-                    //document.querySelector("#seek_by_index_value").innerHTML = frame_index_input.value
+                        //document.querySelector("#seek_by_index_value").innerHTML = frame_index_input.value
                     var pixelesIndex = document.querySelector("#seek_by_index").value / document.querySelector("#seek_by_index").getAttribute("max")
                     document.querySelector("#seek_by_index_value").style.left = (pixelesIndex * 92.5) + "%"
                     document.querySelector("#seek_by_index_value").innerHTML = frame_index_input.value
-                    frame_index_input.addEventListener("change", async () => {
-                        document.querySelector("#seek_by_index_value").innerHTML = frame_index_input.value
-                        seek("seek_to_frame", frame_index_input.value)
-                        frame_time_input.value = frame_index_input.value
-                        document.querySelector("#seek_time_value").innerHTML = (frame_time_input.value / 22000.0).toFixed(6)
-                    })
-                    // Seek to frame by time
+                    frame_index_input.addEventListener("change", async() => {
+                            document.querySelector("#seek_by_index_value").innerHTML = frame_index_input.value
+                            seek("seek_to_frame", frame_index_input.value)
+                            frame_time_input.value = frame_index_input.value
+                            document.querySelector("#seek_time_value").innerHTML = (frame_time_input.value / 22000.0).toFixed(6)
+                        })
+                        // Seek to frame by time
                     frame_time_input.setAttribute("min", 0)
                     frame_time_input.setAttribute("max", (frc - 1))
                     frame_time_input.value = fri
-                    //document.querySelector("#seek_time_value").innerHTML = (frame_time_input.value / 22000.0).toFixed(6)
+                        //document.querySelector("#seek_time_value").innerHTML = (frame_time_input.value / 22000.0).toFixed(6)
                     var pixelesTime = document.querySelector("#seek_time").value / document.querySelector("#seek_time").getAttribute("max")
                     document.querySelector("#seek_time_value").style.left = (pixelesTime * 92.5) + "%"
                     document.querySelector("#seek_time_value").innerHTML = (frame_time_input.value / 22000.0).toFixed(6)
-                    frame_time_input.addEventListener("change", async () => {
+                    frame_time_input.addEventListener("change", async() => {
                         document.querySelector("#seek_time_value").innerHTML = (frame_time_input.value / 22000.0).toFixed(6)
                         seek("seek_to_time", frame_time_input.value / 22000.0)
                         frame_index_input.value = frame_time_input.value
@@ -113,5 +150,3 @@ document.querySelector("#stations").addEventListener("change", () => {
     }
 
 })
-
-
